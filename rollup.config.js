de33cdb9bd72ae770f65afd1f42075a862a446fe
Brandon Ros <brandonros1@gmail.com>
2018-03-22 01:34:27 -0400

    src

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import buble from "rollup-plugin-buble"
+import commonjs from "rollup-plugin-commonjs"
+import resolve from "rollup-plugin-node-resolve"
+import uglify from "rollup-plugin-uglify"
+
+export default {
+  plugins: [
+    buble({ jsx: "h" }),
+    resolve(),
+    commonjs(),
+    uglify()
+  ]
+}
