65847e784998da8d7c17a60cb968e39549048cfd
Brandon Ros <brandonros1@gmail.com>
2018-03-22 14:08:42 -0400

    src

--- src/components/Fields.jsx
+++ src/components/Fields.jsx
@@ -12,16 +12,12 @@ export default ({model, fields, onChange, key}) => (state, actions) => (
           return <br />;
         }
 
-        if (type !== 'static' && render) {
-          debugger
-        }
-
         return (<Field id={id} 
                        type={type} 
                        title={title} 
                        value={model[id]} 
                        options={options} 
-                       render={render ? (type === 'static' ? undefined : () => render({id, key})) : undefined}
+                       render={render && type !== 'static' ? () => render({id, key}) : undefined}
                        disabled={disabled} 
                        style={style}
                        onChange={type === 'static' ? undefined : () => onChange({id, key})} />)
